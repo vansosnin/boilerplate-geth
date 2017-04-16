721c70c0970c89c85796def4b8ad9bbbdd2bea63
Ivan Sosnin <vansosnin@gmail.com>
2017-04-16 19:32:24 +0500

    Update dependencies

--- ui/components/MainPage/components/Layout/Layout.jsx
+++ ui/components/MainPage/components/Layout/Layout.jsx
@@ -1,4 +1,5 @@
-import { PureComponent, PropTypes } from 'react';
+import { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { getTest } from '../../selectors';
